97325f928e383b83128d4fb86d16cd63bc1a79c0
Giuseppe Sarda <giuseppesarda@Giuseppes-Mac-mini.local>
2024-03-17 12:43:53 +0100

    fix bugs

--- keyboards/torn/keymaps/clmcsn/keymap.c
+++ keyboards/torn/keymaps/clmcsn/keymap.c
@@ -35,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT_split_3x6_4(
      KC_GRV,    KC_Q,           KC_W,     LGUI_T(KC_E),         KC_R,             KC_T,     /**/                KC_Y,             KC_U,     RGUI_T(KC_I),         KC_O,       KC_P,     KC_LBRC,
-    KC_UNDS,    KC_A,   LALT_T(KC_S),     LCTL_T(KC_D), LSFT_T(KC_F),             KC_G,     /**/                KC_H,    KRSFT_T(KC_J),     RCTL_T(KC_K), RALT_T(KC_L),    KC_SCLN,     KC_QUOT,
+    KC_UNDS,    KC_A,   LALT_T(KC_S),     LCTL_T(KC_D), LSFT_T(KC_F),             KC_G,     /**/                KC_H,    RSFT_T(KC_J),     RCTL_T(KC_K), RALT_T(KC_L),    KC_SCLN,     KC_QUOT,
      KC_EQL,    KC_Z,           KC_X,             KC_C,         KC_V,             KC_B,     /**/                KC_N,             KC_M,          KC_COMM,       KC_DOT,    KC_SLSH,     KC_RBRC,
-                   LT(_SYST,KC_LEFT), LT(_CURS,KC_ESC),       S_BSPC, LT(_NUMB,KC_DEL),     /**/     LT(_NUMB,G_ENT),            L_SPC, LT(_ITAL,KC_TAB), LT(_MOUS,KC_RGHT)
+                   LT(_SYST,KC_LEFT), LT(_SYST,KC_ESC),       KC_BSPC, LT(_NUMB,KC_DEL),     /**/     LT(_NUMB,KC_ENT),            KC_SPC, LT(_SYST,KC_TAB), LT(_SYST,KC_RGHT)
 ),
 
 /* Number
@@ -53,10 +53,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `---------------------------'    `---------------------------'
  */
 [_NUMB] = LAYOUT_split_3x6_4(
-    KC_CIRC, KC_EXLM,         KC_EQL,    LGUI_T(KC_LT),        KC_GT,            G_ENT,     /**/             KC_PERC,             KC_7,     RGUI_T(KC_8),         KC_9,    KC_COLN,     KC_K,
-    KC_TILD,  KC_DEL,   LALT_T(KC_D),     LCTL_T(KC_E), LSFT_T(KC_F),           KC_TAB,     /**/             KC_PLUS,    KRSFT_T(KC_4),     RCTL_T(KC_5), RALT_T(KC_6),    KC_MINS,     KC_J,
-     KC_DLR,    KC_X,           KC_A,             KC_B,         KC_C,            L_SPC,     /**/             KC_ASTR,             KC_1,             KC_2,         KC_3,    KC_SLSH,     KC_G,
-                             _______,          _______,      _______,          _______,     /**/              KC_DOT,             KC_0,          KC_COMM,       S_BSPC
+    KC_CIRC, KC_EXLM,         KC_EQL,    LGUI_T(KC_LT),        KC_GT,            KC_ENT,     /**/             KC_PERC,             KC_7,     RGUI_T(KC_8),         KC_9,    KC_COLN,     KC_K,
+    KC_TILD,  KC_DEL,   LALT_T(KC_D),     LCTL_T(KC_E), LSFT_T(KC_F),           KC_TAB,     /**/             KC_PLUS,    RSFT_T(KC_4),     RCTL_T(KC_5), RALT_T(KC_6),    KC_MINS,     KC_J,
+     KC_DLR,    KC_X,           KC_A,             KC_B,         KC_C,            KC_SPC,     /**/             KC_ASTR,             KC_1,             KC_2,         KC_3,    KC_SLSH,     KC_G,
+                             _______,          _______,      _______,          _______,     /**/              KC_DOT,             KC_0,          KC_COMM,       KC_BSPC
 ),
 
 /* Symbol
@@ -72,10 +72,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `---------------------------'    `---------------------------'
  */
 [_SYMB] = LAYOUT_split_3x6_4(
-     KC_TAB, KC_LCBR,        KC_QUOT,  LGUI_T(KC_DQUO),        KC_RCBR,          KC_EXLM,     /**/             KC_QUES,          KC_LCBR,  RGUI_T(KC_RCBR),       KC_ASTR,      L_SPC,        G_ENT,
-    KC_HASH, KC_CIRC, LALT_T(KC_EQL),  LCTL_T(KC_UNDS), LSFT_T(KC_DLR),           S_BSPC,     /**/              KC_DEL, KRSFT_T(KC_RPRN),  RCTL_T(KC_RPRN),RALT_T(KC_GRV),     KC_DOT,      KC_TILD,
+     KC_TAB, KC_LCBR,        KC_QUOT,  LGUI_T(KC_DQUO),        KC_RCBR,          KC_EXLM,     /**/             KC_QUES,          KC_LCBR,  RGUI_T(KC_RCBR),       KC_ASTR,      KC_SPC,        KC_ENT,
+    KC_HASH, KC_CIRC, LALT_T(KC_EQL),  LCTL_T(KC_UNDS), LSFT_T(KC_DLR),           KC_BSPC,     /**/              KC_DEL, RSFT_T(KC_RPRN),  RCTL_T(KC_RPRN),RALT_T(KC_GRV),     KC_DOT,      KC_TILD,
       KC_AT,   KC_LT,        KC_BSLS,          KC_MINS,          KC_GT,          KC_SLSH,     /**/             KC_BSLS,          KC_LBRC,          KC_RBRC,       KC_PLUS,    KC_AMPR, LSFT(KC_TAB),
-                              KC_DOT,          KC_PERC,        KC_COLN,          KC_SCLN,     /**/             _______,          _______,          _______,       _______,
+                              KC_DOT,          KC_PERC,        KC_COLN,          KC_SCLN,     /**/             _______,          _______,          _______,       _______
 ),
 /* System
  * ,-----------------------------------------.    ,-----------------------------------------.
@@ -93,7 +93,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_F13,  KC_F10,          KC_F9,  LGUI_T(KC_F8),         KC_F7,          KC_PGUP,     /**/             KC_END , KC_MPRV, _______, KC_MNXT, _______, _______,
      KC_F14,  KC_F11,  LALT_T(KC_F6),  LCTL_T(KC_F5), LSFT_T(KC_F4),          KC_HOME,     /**/             KC_MSTP, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______,
      KC_F15,  KC_F12,          KC_F3,          KC_F2,         KC_F1,          KC_PGDN,     /**/             KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,
-                              ______,        _______,       _______,          _______,     /**/             KC_VOLU, KC_MUTE, KC_VOLD, _______,
+                             _______,        _______,       _______,          _______,     /**/             KC_VOLU, KC_MUTE, KC_VOLD, _______
 )
 
 };
