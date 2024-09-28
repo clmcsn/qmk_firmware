6dc0691e27d7c96252f8c3fcf17d6828c10aad4e
Giuseppe Sarda <giuseppesarda@Giuseppes-Mac-mini.local>
2024-09-28 15:08:49 +0200

    minor layout correct

--- keyboards/torn/keymaps/clmcsn/keymap.c
+++ keyboards/torn/keymaps/clmcsn/keymap.c
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  =   |   Z  |   X  |   C  |   V  |   B  |    |   N  |   M  |   ,  |   .  |   /  |  ]   |
  * |------+------+------+------+------+------|    |------+------+------+------+------+------|
  *               |  <-  | ESC  | Bksp | Del  |    |Enter |Space | TAB  |  ->  |
- *               |System|Cursor|      |Number|    |Symbol|      |Ita   |Mouse |
+ *               |System|Cursor|Number|      |    |Symbol|      |Ita   |Mouse |
  *               `---------------------------'    `---------------------------'
  */
 [_QWERTY] = LAYOUT_split_3x6_4(
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Symbol
  * ,-----------------------------------------.    ,-----------------------------------------.
- * |  Tab |   {  |   '  |LGT(")|   !  |   }  |    |   ?  |   {  |   }  |   *  | Space| Enter|
+ * |  Tab |   {  |   '  |LGT(")|   !  |   @  |    |   ?  |   {  |   }  |   *  | Space| Enter|
  * |------+------+------+------+------+------|    |------+------+------+------+------+------|
  * |   #  |   ^  |LAT(=)|LCT(_)|LST($)| Bksp |    |  Del |   (  |   )  |   `  |   .  |   ~  |
  * |------+------+------+------+------+------|    |------+------+------+------+------+------|
@@ -79,8 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `---------------------------'    `---------------------------'
  */
 [_SYMB] = LAYOUT_split_3x6_4(
-     KC_TAB, KC_LCBR,        KC_QUOT,  LGUI_T(KC_DQUO),        KC_EXLM,          KC_RCBR,     /**/             KC_QUES,          KC_LCBR,  RGUI_T(KC_RCBR),       KC_ASTR,      KC_SPC,        KC_ENT,
-    KC_HASH, KC_CIRC, LALT_T(KC_EQL),  LCTL_T(KC_UNDS), LSFT_T(KC_DLR),           KC_BSPC,     /**/              KC_DEL, RSFT_T(KC_RPRN),  RCTL_T(KC_RPRN),RALT_T(KC_GRV),     KC_DOT,      KC_TILD,
+     KC_TAB, KC_LCBR,        KC_QUOT,  LGUI_T(KC_DQUO),        KC_EXLM,          KC_AT  ,     /**/              KC_LCBR,         KC_QUES,  RGUI_T(KC_RCBR),       KC_ASTR,      KC_SPC,        KC_ENT,
+    KC_HASH, KC_CIRC, LALT_T(KC_EQL),  LCTL_T(KC_UNDS), LSFT_T(KC_DLR),           KC_BSPC,     /**/              KC_DEL, RSFT_T(KC_LPRN),  RCTL_T(KC_RPRN),RALT_T(KC_GRV),     KC_DOT,      KC_TILD,
       KC_AT,   KC_LT,        KC_BSLS,          KC_MINS,          KC_GT,          KC_SLSH,     /**/             KC_BSLS,          KC_LBRC,          KC_RBRC,       KC_PLUS,    KC_AMPR, LSFT(KC_TAB),
                               KC_DOT,          KC_PERC,        KC_COLN,          KC_SCLN,     /**/             _______,          _______,          _______,       _______
 ),
@@ -98,7 +98,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_SYST] = LAYOUT_split_3x6_4(
      KC_F13,  KC_F10,          KC_F9,  LGUI_T(KC_F8),         KC_F7,          KC_PGUP,     /**/             KC_END , KC_MPRV, _______, KC_MNXT, _______, _______,
-     KC_F14,  KC_F11,  LALT_T(KC_F6),  LCTL_T(KC_F5), LSFT_T(KC_F4),          KC_HOME,     /**/             KC_MSTP, KC_MRWD, KC_MPLY, KC_MFFD, _______, _______,
+     KC_F14,  KC_F11,  LALT_T(KC_F6),  LCTL_T(KC_F5), LSFT_T(KC_F4),          KC_HOME,     /**/             KC_MSTP, KC_MRWD, KC_MPLY, KC_MFFD, KC_CAPS, _______,
      KC_F15,  KC_F12,          KC_F3,          KC_F2,         KC_F1,          KC_PGDN,     /**/             KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, _______, _______,
                              _______,        _______,       _______,          _______,     /**/             KC_VOLU, KC_MUTE, KC_VOLD, _______
 )
